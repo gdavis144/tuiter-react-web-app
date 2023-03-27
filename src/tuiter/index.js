a70297ad4b6c0604d43f0ec3f5bf67a335f1ce45
unknown <davis.gi@northeastern.edu>
2023-03-26 22:42:52 -0400

    active tags2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -17,24 +17,39 @@ const store = configureStore({
 function Tuiter() {
     return (
         <Provider store={store}>
+            <Routes>
+                <Route index path="/"
+                       element={
+                           <div className="row mt-2">
+                               <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                                   <NavigationSidebar active={"home"}/>
+                               </div>
+                               <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                                    style={{"position": "relative"}}>
+                                   <HomeComponent/>
+                               </div>
+                               <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                                   <WhoToFollowList/>
+                               </div>
+                           </div>
 
-        <div className="row mt-2">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active={useLocation().pathname.startsWith("/tuiter/explore") ? "explore" : "home"}/>
-            </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
-                 style={{"position": "relative"}}>
-                <Routes>
-                    <Route index path="/"
-                           element={<HomeComponent/>}/>
-                    <Route path="/explore"
-                           element={<ExploreComponent/>}/>
-                </Routes>
-            </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList/>
-            </div>
-        </div>
+                        }/>
+                <Route path="/explore"
+                       element={
+                           <div className="row mt-2">
+                               <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                                   <NavigationSidebar active={"explore"}/>
+                               </div>
+                               <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                                    style={{"position": "relative"}}>
+                                   <ExploreComponent/>
+                               </div>
+                               <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                                   <WhoToFollowList/>
+                               </div>
+                           </div>
+                }/>
+            </Routes>
         </Provider>
     );
 }
